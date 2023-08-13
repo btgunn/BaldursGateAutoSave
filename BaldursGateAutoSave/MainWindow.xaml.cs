f48f6437ba843346936fd0bda755ed4f97a0f831
btgunn <btgunn09@gmail.com>
2023-08-13 02:53:23 -0400

    Added basic functionality. Hardcoded to BG3 & using autoit for sending hotkeys.

--- BaldursGateAutoSave/MainWindow.xaml.cs
+++ BaldursGateAutoSave/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoIt;
+using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using System.Windows.Threading;
 namespace BaldursGateAutoSave
 {
     /// <summary>
@@ -20,9 +11,115 @@ namespace BaldursGateAutoSave
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int saveInterval = 15;
+        private DispatcherTimer? timer;
+        private bool IsRunning = false;
+        private IntPtr gameProcess = IntPtr.Zero;
+        private DateTime lastSaveTime = DateTime.MinValue;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonIncrement_Click(object sender, RoutedEventArgs e)
+        {
+            saveInterval++;
+            UpdateIntervalTextBox();
+        }
+
+        private void ButtonDecrement_Click(object sender, RoutedEventArgs e)
+        {
+            saveInterval--;
+            UpdateIntervalTextBox();
+        }
+        private void UpdateIntervalTextBox()
+        {
+            textBoxInterval.Text = saveInterval.ToString();
+        }
+
+        private void TextBoxInterval_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(textBoxInterval.Text, out int enteredValue))
+            {
+                if (enteredValue >= 0)
+                {
+                    saveInterval = enteredValue;
+                    return;
+                }
+            }
+            
+            UpdateIntervalTextBox();
+        }
+
+        private void ButtonStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsRunning)
+            {
+                //start auto save (press F5) thread/timer
+                StartSaving();
+                textBoxInterval.IsEnabled = false;
+                buttonStartStop.Content = "Stop";
+                IsRunning = true;
+            }
+            else
+            {
+                //Stop auto saving
+                StopSaving();
+                textBoxInterval.IsEnabled = true;
+                buttonStartStop.Content = "Start";
+                IsRunning = false;
+
+            }
+        }
+
+        private void StartSaving()
+        {
+            timer = new DispatcherTimer();
+            
+            gameProcess = GetGameHWND("bg3"); //Currently hardcoded to baldur's gate 3
+
+            if (gameProcess != nint.Zero)
+            {
+                timer.Interval = TimeSpan.FromMinutes(saveInterval);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+        }
+        private void StopSaving()
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer = null;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            AutoItX.WinActivate(gameProcess);
+            AutoItX.Send("{F5}");
+
+            // Update the last save time
+            lastSaveTime = DateTime.Now;
+
+            // Update the label content with the new last save time
+            tbLastSave.Text = $"{lastSaveTime:HH:mm:ss}";
+        }
+
+        //Retrieve hWND for partial game title, used with AutoIt to set focus and send input
+        private IntPtr GetGameHWND(string gameName)
+        {
+            Process[] procs = Process.GetProcesses();
+            foreach (Process proc in procs)
+            {
+                if (proc.ProcessName.Contains(gameName))
+                {
+                    return proc.MainWindowHandle;
+                }
+            }
+
+            return IntPtr.Zero;
         }
     }
 }
